54f08cc61c958640c0dacf16fd7fe55b982e7256
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-10-11 11:34:37 -0300

    Player php

--- includes/Player.php
+++ includes/Player.php
@@ -1,5 +1,24 @@
 <?php
+require_once 'Database.php';
+
 class Player extends Database
 {
+    //table name
+    protected $tableName = 'players';
+
+    public function add($data) {
+
+        if (!empty($data)){
+            $fileds = $placholders = [];
+            foreach ($data as $field => $value){
+                $fileds[] = $field;
+                $placholders[] = ":{$field}";
+            }
+        }
+
+        $sql = "INSERT INTO {$this->tableName} (".implode(',', $fileds).") VALUES(".implode(',', $placholders) .")";
+        $stmt = $this->conn->prepare($sql);
+        
+    }
     
 }
