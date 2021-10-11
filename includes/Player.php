94fe1706e0ab4471026d153c910d416710fc8545
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-10-11 12:53:03 -0300

    Update Player.php

--- includes/Player.php
+++ includes/Player.php
@@ -35,6 +35,16 @@ class Player extends Database
 
     public function getRows($start = 0, $limit = 4) {
         $sql= "SELECT * FROM {$this->tableName} ORDER BY id DESC LIMIT {$start}, {$limit}";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        if($stmt->rowCount() >0){
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } else{
+
+            $results = [];
+        }
+
+        return $results;
     }
     
 }
