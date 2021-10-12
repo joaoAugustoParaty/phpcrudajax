472b7b355d39e92a11aa87e9843e99dd51f3b2e8
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-10-12 11:35:00 -0300

    File Upload function

--- includes/Player.php
+++ includes/Player.php
@@ -83,9 +83,31 @@ class Player extends Database
 
         return $result;
     }
-
+      /** 
+     *function is used to upload file
+     *@param array $file
+     *@return string $newFileName
+    **/
     public function uploadPhoto($file){
-        
+        if(!empty($file)){
+            $fileTempPath = $file['tmp_name'];
+            $fileName = $file['name'];
+            $fileSize = $file['size'];
+            $fileType = $file['type'];
+            $fileNameCmps = explode('.', $fileName);
+            $fileExtension = strtolower(end($fileNameCmps));
+            $newFileName = md5(time().$fileName).'.'. $fileExtension;
+            $allowedExtn = ["jpg", "png", "gif", "jpeg"];
+            if(in_array($fileExtension, $allowedExtn)){
+                $uploadFileDir = getcwd(). '/uploads/';
+                $destFilePath = $uploadFileDir.$newFileName;
+                if (move_uploaded_file($fileTempPath, $destFilePath)){
+                    return $newFileName;
+                }
+            }
+
+
+        }
     }
     
 }
